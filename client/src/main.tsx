f54994958a49bf3dc375de6cafd8b48ee7254721
David Lahu <sysdevlahu918@gmail.com>
2025-10-19 15:36:15 +0100

    Changes for posthog integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,28 @@
+// main.tsx
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App";
 import "./index.css";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    autocapture: true,            // optional, defaults to true
+    disable_session_recording: false, // optional
+  }  
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
+    </StrictMode>
+  );
+}
+
